da19733465a2290595614020ce7e31d3e2559ce9
Bozhao Liu <222460@usn.no>
2019-02-14 12:32:47 +0100

    US-15 Right function

--- VHDL/Cheating_dice/FSM/Cheating_dice_mealy/Cheating_dice_mealy.srcs/sources_1/new/Cheating_dice.vhd
+++ VHDL/Cheating_dice/FSM/Cheating_dice_mealy/Cheating_dice_mealy.srcs/sources_1/new/Cheating_dice.vhd
@@ -89,7 +89,7 @@ begin
         case current_state is 
             when state_1 =>
                 if time_counter = mmswitch then
-                    next_state <= state_2;
+                    next_state <= state_2; --- The Right functionality
                 end if;
                 
             when state_2 =>
